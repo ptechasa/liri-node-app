20d1af89683f85a73e102e9962838064594ff8d9
miko <ptechasa@mail.ccsf.edu>
2019-08-27 17:04:09 -0700

    initial for node js

---
+++ liri.js
@@ -0,0 +1,16 @@
+```js
+require("dotenv").config();
+```
+```js
+  var keys = require("./keys.js");
+```
+```js
+var spotify = new Spotify(keys.spotify);
+```
+`concert-this`
+
+`spotify-this-song`
+
+`movie-this`
+
+`do-what-it-says`
