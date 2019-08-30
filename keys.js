d9e93c405b5c75d81a09a2f57d349c7004aa8d0d
miko <ptechasa@mail.ccsf.edu>
2019-08-30 03:50:40 -0700

    create spotify-this-song

--- keys.js
+++ keys.js
@@ -1,8 +1,10 @@
-```js
 console.log('this is loaded');
 
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
-```
+
+// exports.private = {
+//   id: process.env.PRIVATE_KEY
+// }
--- liri.js
+++ liri.js
@@ -1,16 +1,44 @@
-```js
+// Read and set environment variables
 require("dotenv").config();
-```
-```js
-  var keys = require("./keys.js");
-```
-```js
+var keys = require("./keys.js");
+// console.log(keys)
+// console.log(keys.private.id)
+
+//using Spotify package to retrieve song information
+var Spotify = require('node-spotify-api');
+
+//using axios package to retrieve data from the OMDB API
+var axios = require('axios');
+
+//format date
+var moment = require('moment');
+
+var userCommand = process.argv[2]
+
+// Get all elements in process.argv, which starting from index 3 to the end
+// Join them into a string to get the space delimited userInput
+var userInput = process.argv.slice(3).join(" ")
+
+//grab the fs package to handle read/write
+var filesSystem = require("fs")
+
 var spotify = new Spotify(keys.spotify);
-```
-`concert-this`
 
-`spotify-this-song`
+function spotifyThisSong(userInput) {
+    spotify.search({ type: 'track', query: userInput, limit: 1 }, function (err, data) {
 
-`movie-this`
+        if (err) {
+            console.log('Error', err)
+        } else {
+            // console.log(data.tracks)
+            console.log('Artist(s):', data.tracks.items[0].artists[0].name); 
+            console.log('Song Name:', data.tracks.items[0].name);
+            console.log('Preview song:', data.tracks.items[0].preview_url);
+            console.log('Album:', data.tracks.items[0].album.name)
+        }
+    });
+}
 
-`do-what-it-says`
+
+
+
